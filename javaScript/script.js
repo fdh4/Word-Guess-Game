124a99ebf1bc85ee09db6d593bd6a3127c9591bd
fdh4s GUN <fdh4@aol.com>
2020-03-30 01:56:12 -0700

    static display deployed - text not yet responsive

--- javaScript/script.js
+++ javaScript/script.js
@@ -3,6 +3,9 @@
 
 // arrays
 
+var abcd = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var prob = ["8.167", "1.492", "2.202", "4.253", "12.702", "2.228", "2.015", "6.094", "6.966", "0.153", "1.292", "4.025", "2.406", "6.749", "7.507", "1.929", "0.095", "5.987", "6.327", "9.356", "2.758", "0.978", "2.56", "0.15", "1.994", "0.077"]
+
 var performers = ["Arlo Guthrie",  "Carlos Santana", "Country Joe & The Fish", "Credence Clearwater Revival",
                    "Crosby Stills Nash & Young","The Greatful Dead", "Janis Joplin", 
                    "Jimi Hendrix", "Joan Baez", "Joe Cocker", "Joni Mitchel", "Richie Havens", 
@@ -33,7 +36,10 @@ for (let i = 0; i < performers.length; i++) {
 var song = "./music/" + songs[10];
   console.log(song);
 
-// functions
+// functions 
+
+// function to play sound
+
 function playIt(sound) {
   var x = document.createElement("AUDIO");
     x.setAttribute("src",sound);
@@ -42,12 +48,99 @@ function playIt(sound) {
     x.play();
     document.body.appendChild(x);
 }
+// end of function to play sound
 
-var myguess = "";
+// function to fade text
+
+
+function fadeOut(target) {
+  console.log(target);
+  var sopac = 1;
+  var fopac = 0;
+  var elem = document.getElementById(target);   
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (sopac == fopac) {
+      clearInterval(id);
+    } else {
+      sopac = sopac - 0.005; 
+      elem.style.opacity = sopac; 
+      }
+  }
+}
+
+/* function fadeIn(element) {
+  var duration = 0.5;
+  var interval = 10;//ms
+  var op = 0.0;
+  var iop = element.style.opacity;
+  var timer = setInterval(function () {
+      if (op >= iop) {
+          op = iop;
+          clearInterval(timer);
+      }
+      element.style.opacity = op;
+      op += op/((1000/interval)*duration);
+  }, interval);
+} */
+
 // procedures
 
 $(document).ready(function() {
 
-  playIt(song);
+  // choose artist name
 
+  // compute difficulty based on letter frequency arrays
+
+  // compute number of guesses incorporating name length and degree of difficulty
+
+  // set parameters
+      // start slide shows
+        // banner images (artists)
+        // center images (crowd)
+
+  // initalize
+    // start woodstock song
+    // put word to guess template in word container
+    // display  remaining guesses
+    // display wins
+    //display attempst
+    // display 'Press any key to get started!' over center image
+
+  // on key stroke
+    // remove msg over center image
+    // ignore duplicates
+    // if miss 
+      // add to keys already guessed
+      // decrement guesses left
+      // if guesses left = 0 and word incomplete, new game
+      // if guesses left > 0 play buzzer
+      // await user input
+
+    // if hit
+      // play ding sound
+      // display letter in word to guess conainer
+      // if word to guess complete
+        // play ding sound,
+        //display artists picture
+        //play artists song
+        // new game
+      // if word to guess incomplete
+      // show 'Press another key'  message
+      // await user input
+
+    //var clabcd = abcd.indexOf("c");
+    //console.log(abcd.indexOf("c"));
+    //console.log(prob[clabcd]);
+    // console.log(prob);
+
+    document.onkeyup = function(event) {
+      fadeOut("start"); 
+    };
+     console.log("*");
+    playIt(song);
+      console.log("**");
+/*     fadeIn("start");
+    console.log("**");
+ */
 });
